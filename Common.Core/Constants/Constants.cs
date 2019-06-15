9e57e97f07140cbc46b9ad5c3ffabc6117fc6465
tambours <tambours@TAMBOURS7>
2019-06-15 09:55:43 -0300

    Correccion de carpetas

--- Common.Core/Constants/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Common.Core.Constants
-{
-    public static class Constants
-    {
-        public const decimal IVA = 0.21M;
-    }
-}
--- Common.Core/Model/Localidad.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Common.Core.Model
-{
-    public class Localidad : Entity<int>
-    {
-        public string Descripcion { get; internal set; }
-        public bool Habilitado { get; internal set; }
-    }
-}
